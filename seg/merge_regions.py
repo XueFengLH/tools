9dc4ba3ff7621e2b45f6f388f68bbb108ef4a403
ksn <kkkk.ng@foxmail.com>
2024-07-02 14:28:37 +0800

    seg_data_merge2

--- seg/merge_regions.py
+++ seg/merge_regions.py
@@ -2,8 +2,9 @@ from shapely.geometry import Polygon
 import json
 import os
 from tqdm import tqdm
-def qz()
-rounded_list = [round(num) for num in original_list]
+def int_list(l):
+    rounded_list = [round(num) for num in l]
+    return rounded_list
 def r_json(file_path):
 
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -11,31 +12,42 @@ def r_json(file_path):
     shapes = data['shapes']
     n = len(shapes)
     if n > 1:
-        print(file_path)
-        print(data)
+        # print(file_path)
+        # print(data)
         # print(data)
         b = 1
         while b > 0:
             n = len(shapes)
             if n == 1:
                 break
+            b1 = 0
             b = 0
-            for i,shape1 in enumerate(shapes):
-                for j,shape2 in enumerate(shapes[i+1:]):
-                    polygon1_points = [tuple(int(point)) for point in shape1['points']]
-                    polygon2_points = [tuple(int(point)) for point in shape2['points']]
+            for i in range(n):
+                if i >= n - b1:
+                    break
+                for j in range(i+1,n):
+                    if i + j >= n - b1:
+                        break
+                    shape1 = shapes[i]
+                    shape2 = shapes[j]
+                    polygon1_points = [tuple(int_list(point)) for point in shape1['points']]
+                    polygon2_points = [tuple(int_list(point)) for point in shape2['points']]
                     polygon1 = Polygon(polygon1_points)
                     polygon2 = Polygon(polygon2_points)
-                    merged_polygon = polygon1.union(polygon2)
+                    try:
+                        merged_polygon = polygon1.union(polygon2)
+                    except:
+                        continue
                     if merged_polygon.area < polygon1.area + polygon2.area:
                         try:
                             merged_polygon_points = list(merged_polygon.exterior.coords)
                         except:
                             continue
                         shapes[i]['points'] = merged_polygon_points
-                        del shapes[i+j+1]
+                        del shapes[i+j]
+                        b1 = b1 + 1
+                        # print(i+j)
                         b = 1
-                        print(1)
         data['shapes'] = shapes
         file.close()
         with open(file_path , 'w', encoding='utf-8') as file1:
@@ -44,7 +56,7 @@ def r_json(file_path):
 
 
 if __name__ == '__main__':
-    path = '/mnt/sda2/test_seg/json_labels'
+    path = '/mnt/sda2/share/json_labels'
     dir_list = os.listdir(path)
     for dir in tqdm(dir_list):
         file_path = os.path.join(path, dir)
